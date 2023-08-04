feb08d347ad44ef39c1e912c09c51358d4018b80
Jose <jrdeavila@unicesar.edu.co>
2023-08-04 07:25:20 -0500

    Bug de conexion arreglado

--- lib/controllers/connection_ctrl.dart
+++ lib/controllers/connection_ctrl.dart
@@ -1,5 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
+import 'package:music_concept_app/lib.dart';
 
 class ConnectionCtrl extends GetxController {
   final _connectivity = Connectivity();
@@ -8,14 +9,25 @@ class ConnectionCtrl extends GetxController {
   @override
   void onReady() {
     super.onReady();
-    _checkConnection();
     _connectivityResult.bindStream(_connectivity.onConnectivityChanged);
+    _checkConnection();
   }
 
   void _checkConnection() {
     _connectivityResult.listen((p0) {
       if (p0 == ConnectivityResult.none) {
+        Get.delete<AuthenticationCtrl>();
         Get.offAndToNamed("/not-wifi");
+      } else {
+        Get.put(AuthenticationCtrl());
+        Get.put(AppModeCtrl());
+        Get.put(NotificationCtrl());
+        Get.lazyPut(() => LoginCtrl());
+        Get.lazyPut(() => RegisterCtrl());
+        Get.lazyPut(() => ResetPasswordCtrl());
+        Get.lazyPut(() => RegisterBussinessCtrl());
+        Get.lazyPut(() => FanPageCtrl());
+        Get.offAllNamed("/");
       }
     });
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -10,13 +11,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+
   Get.put(ConnectionCtrl());
-  Get.put(AuthenticationCtrl());
-  Get.put(AppModeCtrl());
-  Get.lazyPut(() => LoginCtrl());
-  Get.lazyPut(() => RegisterCtrl());
-  Get.lazyPut(() => ResetPasswordCtrl());
-  Get.lazyPut(() => RegisterBussinessCtrl());
   runApp(const MyApp());
 }
 
