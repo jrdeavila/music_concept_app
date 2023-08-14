9664cdddc4f342a1ccee3d1a192ea911b9ad42b4
Jose <jrdeavila@unicesar.edu.co>
2023-08-14 17:35:45 -0500

    Vista de No Conexion

--- lib/controllers/connection_ctrl.dart
+++ lib/controllers/connection_ctrl.dart
@@ -5,8 +5,11 @@ import 'package:get/get.dart';
 import 'package:music_concept_app/lib.dart';
 
 class ConnectionCtrl extends GetxController {
+  final _pining = Rx<bool>(false);
   final _connectivityResult = Rx<bool?>(null);
   final RxString _lastRoute = "/".obs;
+
+  bool get pining => _pining.value;
 
   @override
   void onReady() {
@@ -32,22 +35,33 @@ class ConnectionCtrl extends GetxController {
     });
   }
 
-  void _checkInternetAccess() {
-    Timer.periodic(const Duration(seconds: 10), (timer) async {
-      try {
-        final result = await Process.run('ping', ['-c', '1', 'google.com']);
+  Future<void> _request() async {
+    try {
+      final result = await Process.run('ping', ['-c', '1', 'google.com']);
 
-        if (result.exitCode == 0) {
-          // Ping exitoso, hay internet
-          _connectivityResult.value = true;
-        } else {
-          // No hay internet
-          _connectivityResult.value = false;
-        }
-      } catch (e) {
+      if (result.exitCode == 0) {
+        // Ping exitoso, hay internet
+        _connectivityResult.value = true;
+      } else {
         // No hay internet
         _connectivityResult.value = false;
       }
+    } catch (e) {
+      // No hay internet
+      _connectivityResult.value = false;
+    }
+  }
+
+  void ping() async {
+    _pining.value = true;
+    Future.delayed(const Duration(seconds: 5), () {
+      _request().then((value) => _pining.value = false);
+    });
+  }
+
+  void _checkInternetAccess() {
+    Timer.periodic(const Duration(seconds: 10), (timer) async {
+      await _request();
     });
   }
 }
