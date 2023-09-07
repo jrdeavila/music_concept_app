71d4a7918675680a302976ddf5958a7ab7e44dd0
Jose De avila <jrdeavila@unicesar.edu.co>
2023-09-07 09:53:16 -0500

    Actualizar API Keys de Maps y Places

--- lib/config/defaults.dart
+++ lib/config/defaults.dart
@@ -3,7 +3,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 abstract class AppDefaults {
   static const titleName = "BeatConnect";
   static const firebaseStorageBucket = "gs://music-concept-app.appspot.com";
-  static const googleApiKeyPlaces = "AIzaSyBUNFC00uy1iSenhDyp_mISgLl30Z3lYyU";
+  static const googleApiKeyPlaces = "AIzaSyA49opAaeHWJTYkHbSy5VHUoD63Q38w3RM";
   static const defaultPositon = LatLng(
     10.4634,
     -73.2532,
